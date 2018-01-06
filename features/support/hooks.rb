bc05b13e323387ebf800816cb8e81d2651bc73f3
Stephan Schwab <sns@caimito.net>
2018-01-06 17:50:34 +0100

    screenshot embed

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -4,6 +4,7 @@ After do |scenario|
     name_of_scenario = time + scenario.name.gsub(/\s+/, "_").gsub("/","_")
     file_path = "#{SCREENSHOTS_DIR}/#{name_of_scenario}.png"
     @browser.screenshot.save file_path
+    embed file_path, 'image/png'
   end
 end
 
